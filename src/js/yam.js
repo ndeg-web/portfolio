c14dc6fe0fd54be7775fa05731d6919a6c06a65d
hen04 <nadia_degtiareva@rambler.ru>
2025-10-06 20:04:26 +0300

    metrika

--- src/js/yam.js
+++ src/js/yam.js
@@ -1,12 +1,22 @@
-(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-m[i].l=1*new Date();
-for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+// (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+// m[i].l=1*new Date();
+// for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+// k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+// (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-ym(98480638, "init", {
-    clickmap:true,
-    trackLinks:true,
-    accurateTrackBounce:true,
-    webvisor:true
-});
+// ym(98480638, "init", {
+//     clickmap:true,
+//     trackLinks:true,
+//     accurateTrackBounce:true,
+//     webvisor:true
+// });
+
+
+(function(m,e,t,r,i,k,a){
+    m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+    m[i].l=1*new Date();
+    for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+    k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+})(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=104425906', 'ym');
+
+ym(104425906, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});
